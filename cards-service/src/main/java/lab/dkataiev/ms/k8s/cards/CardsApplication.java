047a660eac2cecab55f838f77d62db6fdc8115f1
Dmytro Kataiev <d.kataiev@gmail.com>
2021-09-27 12:03:19 +0300

    Add RefershScope

--- cards-service/src/main/java/lab/dkataiev/ms/k8s/cards/CardsApplication.java
+++ cards-service/src/main/java/lab/dkataiev/ms/k8s/cards/CardsApplication.java
@@ -3,11 +3,13 @@ package lab.dkataiev.ms.k8s.cards;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScans({
         @ComponentScan("lab.dkataiev.ms.k8s.cards.config"),
         @ComponentScan("lab.dkataiev.ms.k8s.cards.repository"),
